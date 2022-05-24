d606edb6c3a68876801ecb3f4b110c9a69c8a157
jheart-vic <adebowalevictorjheart.com>
2022-05-24 18:12:50 +0100

    Made required changes

---
+++ index.js
@@ -0,0 +1,31 @@
+const [navList, navAdd, navContact] = document.querySelectorAll('.list-item');
+const allBooks = document.querySelector('.allBooks');
+const addBook = document.querySelector('.addBook');
+const contact = document.querySelector('.contact');
+const show = document.querySelector('.show');
+const display = document.querySelector('.display-tex');
+
+
+navList.addEventListener('click', () => {
+  allBooks.classList.remove('hidden');
+  show.classList.remove('hidden');
+  display.classList.remove('hidden');
+  addBook.classList.add('hidden');
+  contact.classList.add('hidden');
+});
+
+navAdd.addEventListener('click', () => {
+  addBook.classList.remove('hidden');
+  allBooks.classList.add('hidden');
+  contact.classList.add('hidden');
+  show.classList.add('hidden')
+  display.classList.add('hidden')
+});
+
+navContact.addEventListener('click', () => {
+  contact.classList.remove('hidden');
+  allBooks.classList.add('hidden');
+  show.classList.add('hidden')
+  addBook.classList.add('hidden');
+  display.classList.add('hidden');
+});
